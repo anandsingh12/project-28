8b9992370e8e5fe9e7cdd15492a7320829a0fc85
anandsingh12 <77194646+anandsingh12@users.noreply.github.com>
2021-03-03 13:44:21 +0530

    Add files via upload

---
+++ mangoProjectTemplate-main/sketch.js
@@ -0,0 +1,66 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1,mango2,mango3,mango4,mango5,mango6;
+var world,boy;
+
+function preload(){
+	boy=loadImage("images/boy.png");
+	
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+	stone = new Stone(250,420,40);
+	
+	mango1=new mango(1100,100,30);
+	mango2=new mango(1200,200,30);
+	mango3=new mango(1000,200,30);
+	mango4=new mango(1150,200,30);
+	mango5=new mango(980,100,30)
+    mango6=new mango(940,250,30);
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	chain = new Chain(stone.body,{x:250, y:420});
+	
+
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+
+  treeObj.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  mango6.display();
+  groundObject.display();
+  chain.display();
+ stone.display();
+ 
+
+ 
+  drawSprites();
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(stone.body,{x:mouseX, y:mouseY});
+}
+function mouseReleased(){
+    chain.fly();
+}
